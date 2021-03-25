ae3044b9764f025ea774942df3b6837cf67f93e6
myizci <izci@hotmail.com>
2021-03-25 10:31:35 -0400

    first commit

--- src/test/java/day1/JUnit5_Intro.java
+++ src/test/java/day1/JUnit5_Intro.java
@@ -1,11 +1,13 @@
 package day1;
 
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
 public class JUnit5_Intro {
 
     @Test
     public void test(){
         System.out.println("Learning JUnit5");
+
+        assertEquals(1,1,"Something is wrong");
     }
 }
